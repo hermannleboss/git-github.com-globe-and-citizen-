16a2af41b1055a8b4f8c57d9567fbb18aa9e3af3
Hermann EHO <hermannleboss@gmail.com>
2022-11-11 12:07:12 +0000

    fix: fix title colors and link text

--- pages/components/nftCard.tsx
+++ pages/components/nftCard.tsx
@@ -6,12 +6,12 @@ export const NFTCard: NextPage = ({ nft }) => {
       <div className="rounded-md  min-h-128">
         <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} alt={nft.description.substring(0, 10)}></img>
       </div>
-      <h2 className="text-4xl hover:text-cyan-500 my-6">{nft.title}</h2>
+      <h2 className="text-4xl hover:text-cyan-400 my-6">{nft.title}</h2>
       <p className="text-lg text-indigo-400 mb-6">{nft.description.substring(0, 80)}</p>
       <p className="text-base">Id: {nft.id.tokenId.substring(nft.id.tokenId.length - 4)}</p>
       <p className="mb-6">Contract: {nft.contract.address.substring(0, 4)}
         ... {nft.contract.address.substring(nft.contract.address.length - 4)}</p>
-      <div><a target={"_blank"} href={`https://etherscan.io/token/${nft.contract.address}`}>Viex on ehterscan</a></div>
+      <div className="hover:text-cyan-400"><a target={"_blank"} href={`https://etherscan.io/token/${nft.contract.address}`}>Views on ehterscan</a></div>
     </div>
   )
 }
